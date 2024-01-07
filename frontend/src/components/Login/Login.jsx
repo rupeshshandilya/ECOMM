c94bd4a3d5f034ceff0ca8a823d0ced04faca418
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-01-08 02:56:00 +0530

    feat(Login.jsx): add new Login component for user authentication
    This commit introduces a new Login component that allows users to authenticate themselves. The component includes form fields for email and password, a 'Remember Me' checkbox, a 'Forgot Password?' link, and a 'Signup' link for new users. It also includes a visibility toggle for the password field.

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,116 @@
+import { React, useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import styles from "../../styles/styles";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <div className="bg-white p-10 rounded-lg shadow-md">
+          <img
+            src="https://demos.themeselection.com/sneat-mui-react-nextjs-admin-template/demo-1/images/pages/boy-with-rocket-light.png"
+            alt="Boy with rocket"
+            className="w-48 mx-auto"
+          />
+          <h2 className="text-2xl font-bold text-center mt-4 mb-2">
+            Welcome to E-SHOP!
+          </h2>
+          <p className="text-sm text-center mb-8">
+          Login your account to discover exclusive deals.
+          </p>
+
+          <form>
+            {/* Form fields */}
+            <div className="mb-4">
+              <label htmlFor="email" className="sr-only">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                autoComplete="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-lg"
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="relative mb-4">
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                type={visible ? "text" : "password"}
+                name="password"
+                autoComplete="current-password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-lg"
+                id="password"
+                placeholder="Password"
+              />
+              {visible ? (
+                <AiOutlineEye
+                  className="absolute inset-y-2 right-2"
+                  size={25}
+                  onClick={() => setVisible(false)}
+                />
+              ) : (
+                <AiOutlineEyeInvisible
+                  className="absolute inset-y-2 right-2"
+                  size={25}
+                  onClick={() => setVisible(true)}
+                />
+              )}
+            </div>
+            <div className="flex items-center justify-between mb-6">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-blue-600"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 text-sm text-gray-600"
+                >
+                  Remember Me
+                </label>
+              </div>
+              <a
+                href=".forgot-password"
+                className="text-sm text-blue-600 hover:underline"
+              >
+                Forgot Password?
+              </a>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600"
+            >
+              Login
+            </button>
+            {/* ============================== Create Account ============================= */}
+            <p className="mt-4 text-sm text-center">
+              New on our platform?
+              <Link to="/signup" className="text-blue-600 pl-1">
+                Signup
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
