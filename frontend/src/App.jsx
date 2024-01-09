bb469100259b250bb1599da696259299d9005c2f
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-01-10 01:45:32 +0530

    feat(App.jsx): add SignupPage route to enable user registration functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import { LoginPage } from './Routes';
+import { LoginPage, SignupPage } from './Routes';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<LoginPage />}/>
+        <Route path='/signup' element={<SignupPage />}/>
       </Routes>
     </BrowserRouter>
   )
