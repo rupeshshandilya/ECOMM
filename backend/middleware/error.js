a7e6f02768637fbfa564ebf1748346769a092d7b
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-01-10 02:45:32 +0530

    feat(middleware): add new catchAsyncError.js file for handling async errors refactor(error.js): improve code readability by adding spaces and semicolons fix(error.js): correct error messages for better user understanding style(error.js): replace single quotes with double quotes for string consistency

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,36 +1,35 @@
-const ErrorHandler = require('../utils/ErrorHandler');
+const ErrorHandler = require("../utils/ErrorHandler");
 
-module.exports = (err,req,res,next)=>{
-    err.statusCode = err.statusCode || 500,
-    err.message = err.message || 'Internal Server Error'
+module.exports = (err, req, res, next) => {
+  (err.statusCode = err.statusCode || 500),
+    (err.message = err.message || "Internal Server Error");
 
+  //Mongo Db id Error
+  if (err.name === "CastError") {
+    const message = `Resource not found with this id... ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
 
-    //Mongo Db id Error
-    if(err.name === "CastError"){
-        const message = `Resource not found with this id... ${err.path}`;
-        err = new ErrorHandler(message,400);
-    }
+  //Duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate Key ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
 
-    //Duplicate key error
-    if(err.code === 11000){
-        const message = `Duplicate Key ${Object.keys(err.keyValue)} Entered`;
-        err = new ErrorHandler(message,400)
-    }
+  //authentication error (jwt)
+  if (err.name === "JsonWebTokenError") {
+    const message = `Your Url is invalid please try again later`;
+    err = new ErrorHandler(message, 400);
+  }
 
-    //authentication error (jwt)
-    if(err.name === "JsonWebTokenError"){
-        const message = `Your Url is invalid please try again later`;
-        err = new ErrorHandler(message,400)
-    }
+  //jwt expired
+  if (err.name === "TokenExpiredError") {
+    const message = `Your Url is Expired please try again later`;
+    err = new ErrorHandler(message, 400);
+  }
 
-    //jwt expired
-    if(err.name === "TokenExpiredError"){
-        const message = `Your Url is Expired please try again later`;
-        err = new ErrorHandler(message,400);
-    }
-
-    res.status(err.statusCode).json({
-        success: false,
-        message: err.message,
-    })
-}
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
