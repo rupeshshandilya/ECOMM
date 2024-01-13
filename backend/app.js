9a42b650ff1fcdfc12de2dcbc302421be2375882
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-01-13 23:14:01 +0530

    feat(app.js): add new middleware for error handling, cookie parsing, body parsing, and file uploading - ErrorHandler for better error management - cookieParser to parse cookies - bodyParser to parse request bodies - fileUpload to handle file uploads with temp files option enabled for better file management

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const ErrorHandler = require("./utils/ErrorHandler");
+const cookieParser = require("cookie-parser")
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({useTempFiles: true}))
+
 
 /* ============================== CONFIG ============================= */
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -7,5 +17,9 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     path: "backend/config/.env",
   });
 }
+
+/* ============================== FOR ERRORHANDLING ============================= */
+app.use(ErrorHandler)
+
 
 module.exports = app;
