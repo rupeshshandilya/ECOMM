b0a1572d4abc3dff0019868b8fca266eef399bf1
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-01-15 21:56:32 +0530

    fix(multer.js): correct function parameters in destination and filename methods fix(multer.js): fix typo in file.originalname and split method call to correctly generate filename feat(multer.js): add uniqueSuffix to filename to avoid file name collisions

--- backend/multer.js
+++ backend/multer.js
@@ -1,13 +1,12 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, res, cb) {
+  destination: function (req, file, cb) {
     cb(null, "uploads/");
   },
-  filename: function (req, res, cb) {
-    const uniqueSuffix =
-      Date.now() + "-" + Math.round.apply(Math.random() * 1e9);
-    const filename = file.orignalname.spl(".")[0];
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const filename = file.originalname.split(".")[0];
     cb(null, filename + "-" + uniqueSuffix + ".png");
   },
 });
