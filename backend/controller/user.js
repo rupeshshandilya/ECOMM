a8223845558efbae32e6afa54979c663880340ac
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-01-18 23:24:45 +0530

    feat(user.js): add email verification on user registration: The changes include adding a feature to send an activation email to the user upon registration. This is done by creating an activation token and sending it to the user's email. The user can then activate their account by clicking on the link in the email.

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const path = require('path')
-const router = express.Router()
-const {upload} = require('../multer')
-const User = require('../model/user')
-const ErrorHandler = require('../utils/ErrorHandler')
-const fs = require("fs")
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const { upload } = require("../multer");
+const User = require("../model/user");
+const ErrorHandler = require("../utils/ErrorHandler");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const sendMail = require("../utils/sendMail");
 
+router.post("/create-user", upload.single("file"), async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const userEmail = await User.findOne({ email });
 
-router.post("/create-user", upload.single("file"), async (req,res, next)=>{
-    const {name,email,password} = req.body;
-    const userEmail = await User.findOne({email});
-
-    if(userEmail){
-        //Stop Uploading picture if user already registered
-        const filename = req.file.filename;
-        const fielpath = `uploads/${filename}`;
-        fs.unlink(fielpath, (err)=>{
-            if(err){
-                res.status(500).json({message: "Error in deleting file"});
-            } else{
-                res.json({message: "file deleted successfully"})
-            }
-        });
-        return next(new ErrorHandler("User already exists",400))
+    if (userEmail) {
+      //Stop Uploading picture if user already registered
+      const filename = req.file.filename;
+      const fielpath = `uploads/${filename}`;
+      fs.unlink(fielpath, (err) => {
+        if (err) {
+          res.status(500).json({ message: "Error in deleting file" });
+        } else {
+          res.json({ message: "file deleted successfully" });
+        }
+      });
+      return next(new ErrorHandler("User already exists", 400));
     }
 
     //if user not exist
@@ -30,18 +32,45 @@ router.post("/create-user", upload.single("file"), async (req,res, next)=>{
     const fileUrl = path.join(filename);
 
     const user = {
-        name: name,
-        email: email,
-        password: password,
-        avatar: fileUrl,        
+      name: name,
+      email: email,
+      password: password,
+      avatar: fileUrl,
     };
 
     //uploading data to database
-    const newUser = await User.create(user);
-    res.status(201).json({
+    // const newUser = await User.create(user);
+    // res.status(201).json({
+    //     success: true,
+    //     newUser,
+    // });
+    const activationToken = createActivationToken(user);
+
+    const activationUrl = `http://localhost:5173/activation/${activationToken}`;
+
+    try {
+      await sendMail({
+        email: user.email,
+        subject: "Activate your account",
+        message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+      });
+      res.status(201).json({
         success: true,
-        newUser,
+        message: `please check your email:- ${user.email} to activate your account!`,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  } catch (err) {
+    return next(new ErrorHandler(err.message, 400));
+  }
+});
+
+const createActivationToken = (user) => {
+    return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+        //after 10min user can't verify mail 
+        expiresIn: "10m",
     })
-})
+}
 
 module.exports = router;
